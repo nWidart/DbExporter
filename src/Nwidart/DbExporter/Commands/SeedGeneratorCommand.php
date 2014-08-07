878aa108bb4444db63646295ebf4033b59f262aa
Nicolas Widart <n.widart@gmail.com>
2014-08-07 11:03:18 +0200

    Add ability to ignore tables with the dbe:seed command.

--- src/Nwidart/DbExporter/Commands/SeedGeneratorCommand.php
+++ src/Nwidart/DbExporter/Commands/SeedGeneratorCommand.php
@@ -1,7 +1,10 @@
 <?php namespace Nwidart\DbExporter\Commands;
 
 
-use Nwidart\DbExporter\DbExportHandler, Str, Config;
+use Nwidart\DbExporter\DbExporter;
+use Nwidart\DbExporter\DbExportHandler;
+use Symfony\Component\Console\Input\InputOption;
+use Config, Str;
 
 class SeedGeneratorCommand extends GeneratorCommand
 {
@@ -25,7 +28,18 @@ class SeedGeneratorCommand extends GeneratorCommand
     {
         $this->comment("Preparing the seeder class for database {$this->getDatabaseName()}");
 
-        $this->handler->seed();
+        // Grab the options
+        $ignore = $this->option('ignore');
+
+        if (empty($ignore)) {
+            $this->handler->seed();
+        } else {
+            $tables = explode(',', str_replace(' ', '', $ignore));
+            $this->handler->ignore($tables)->seed();
+            foreach (DbExporter::$ignore as $table) {
+                $this->comment("Ignoring the {$table} table");
+            }
+        }
 
         // Symfony style block messages
         $formatter = $this->getHelperSet()->get('formatter');
@@ -41,5 +55,12 @@ class SeedGeneratorCommand extends GeneratorCommand
     {
         $filename = Str::camel($this->getDatabaseName()) . "TableSeeder";
         return Config::get('db-exporter::export_path.seeds')."{$filename}.php";
+    }
+
+    protected function getOptions()
+    {
+        return array(
+            array('ignore', 'ign', InputOption::VALUE_REQUIRED, 'Ignore tables to export, separated by a comma', null)
+        );
     }
 }
